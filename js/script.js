fa42bafa0fd1722bb76d28043e9b3dc37d037ceb
s(a/e)nd (f/b)ox <97696791+ptrzgo@users.noreply.github.com>
2022-03-14 14:51:50 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    $('.logo').on("click", function(){
+        $(this).css('background-color','black');
+    })
+});$('.post-content h1').hide ();
